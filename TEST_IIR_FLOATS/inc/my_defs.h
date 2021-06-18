1458c5a6432d9b9ffd992f573f1ba6c3f9b80e21
Aïcha <a.diez22@usp.ceu.es>
2021-06-18 13:41:35 +0200

    Update my_defs.h

--- TEST_IIR_FLOATS/inc/my_defs.h
+++ TEST_IIR_FLOATS/inc/my_defs.h
@@ -1,7 +1,8 @@
 #ifndef MY_DEF_H
 #define MY_DEF_H
 
-#include "elliptical_IIR.h"
+#include "float32.h"
+#include "notch32.h"
 
 // PIT timer parameters
 #define F_CLOCK (24e6)
@@ -10,10 +11,9 @@
 
 
 // PIN information
-#define MASK(x) (1UL << (x))
-#define RED_LED_POS (18)		// on port B
+#define MASK1(x) (1UL << (x))
+#define RED_LED_POS (18)	// on port B
 #define GREEN_LED_POS (19)	// on port B
-#define SWITCH_POS (0)	// on port B
 
 // AND: Channel ADC0_SE3 is PTE22
 #define ADC_CHANNEL (3) 
@@ -22,37 +22,26 @@
 // READ_SYNTH_SAMPLE parameters
 #define F_BEAT (55./60.)		// beat frequency in Hz (beats per minute/60)
 #define SYNTH_PERIOD	((int)(F_SAMPLING/F_BEAT))	// this is for the read_sensor() function
-																				    // every PERIOD cycles a new beat will be synthesized
+							 // every PERIOD cycles a new beat will be synthesized
 // Beat detection parameters
-#define BEAT_THRESHOLD (60)
+#define BEAT_THRESHOLD (4)
 
-#define ABS_BW (16)												// absolute value of the maximum for bias wandering [-ABS_BW,+ABS_BW]
-#define PEAK_VAL (64)											// peak value
-#define PEAK_WIDTH (3)										// peak width
+#define ABS_BW (16)	// absolute value of the maximum for bias wandering [-ABS_BW,+ABS_BW]
+#define PEAK_VAL (64)	// peak value
+#define PEAK_WIDTH (3)  // peak width
 
 
 // BUFFERS
 // Size of arrays
-#define Mb (NUMERATOR_TAPS)	// Number of 	numerator coefficients
-#define Ma (DENOM_TAPS)		// Number of denumerator coefficients
+#define Mb (NUMERATOR_TAPS)	// Number of 	numerator coefficients of iir filter
+#define Ma (DENOMINATOR_TAPS)	// Number of denumerator coefficients of iir filter
+#define Na (NOTCH_NUMERATOR)	// Number of 	numerator coefficients of notch filter
+#define Nb (NOTCH_DENOMINATOR)	// Number of 	denumerator coefficients of notch filter
 
-#define OSZ (1000)						// Size of the ouput buffer
-#define ISZ Mb+1	    					// the size of the input vector must be at least equal to the size of h(n)
-#define SIZE_OF_BUFFER 10
+#define ISZ Mb+1	    					// the size of the input vector must be at least equal to the size of b(n)
+#define INSZ Nb+1							 // the size of the notch input vector (output1) must be at least equal to the size its b(n)
+#define OSZ (100)						// Size of the ouput buffer
 
-/*	-----------> JORGE THINGS <-------------
-#define LDV 23900 							// it controls the sampling frequency. we put here the velocity
-#define MAX_COEFF 100						// no. of FIR  coefficients (Used in Jorge's tests)
-
-// ANS: LDV = round(T*fcount - 1) = round (1 ms*24 Mhz -1) (Unit 6: slide 14)
-//      LDV = round(1e-3*24e6 -1 )=round(24000-1)=23999
-//			fs=  1 KHz -> 23999
-//      fs= 10 KHz -> 2399
-//			fs=100 KHZ -> 239
-//			fs=200 KHZ -> 119 (about the limit)
-//      check also (UNIT 6: slide 13)
-
-*/
 
 
 #endif
